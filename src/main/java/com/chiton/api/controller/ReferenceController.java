c55a89464133a20b5097cc6a7942036a7ef6dbad
Joluvc01 <joluvc01@gmail.com>
2024-01-23 16:16:01 -0500

    Este vez si Reference Controller

--- src/main/java/com/chiton/api/controller/ReferenceController.java
+++ src/main/java/com/chiton/api/controller/ReferenceController.java
@@ -65,20 +65,47 @@ public class ReferenceController {
         newReference.setDescription(referenceDTO.getDescription());
         newReference.setImage(referenceDTO.getImage());
 
-        // Validar la existencia de los productos en los detalles y manejar duplicados
-        Map<String, ReferenceDetail> productDetailsMap = new HashMap<>();
-        ResponseEntity<?> detailDTO = handleReferenceDetails(referenceDTO, newReference, productDetailsMap);
-        if (detailDTO != null) return detailDTO;
+        // Mapa para realizar un seguimiento de los detalles del JSON por producto
+        Map<String, ReferenceDetailDTO> productDetailsMap = new HashMap<>();
 
-        // Asociar los detalles a la referencia y guardar todo dentro de una transacción
-        List<ReferenceDetail> referenceDetails = new ArrayList<>(productDetailsMap.values());
+        // Combinar cantidades de detalles con el mismo producto
+        for (ReferenceDetailDTO detailDTO : referenceDTO.getDetails()) {
+            String productName = detailDTO.getProduct();
+            if (productDetailsMap.containsKey(productName)) {
+                // Si ya existe, sumar la cantidad al detalle existente
+                ReferenceDetailDTO existingDetailDTO = productDetailsMap.get(productName);
+                existingDetailDTO.setQuantity(existingDetailDTO.getQuantity() + detailDTO.getQuantity());
+            } else {
+                // Si no existe, agregar el detalle al mapa
+                productDetailsMap.put(productName, detailDTO);
+            }
+        }
+
+        // Asociar los detalles a la referencia
+        List<ReferenceDetail> referenceDetails = new ArrayList<>();
+
+        for (ReferenceDetailDTO detailDTO : productDetailsMap.values()) {
+            Product product = productService.findByName(detailDTO.getProduct());
+            if (product == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("El producto " + detailDTO.getProduct() + " en los detalles no existe");
+            }
+
+            ReferenceDetail newDetail = convertDTO.convertToReferenceDetail(detailDTO, newReference);
+            newDetail.setProduct(product);
+            referenceDetails.add(newDetail);
+        }
+
         newReference.setDetail(referenceDetails);
+
+        // Guardar todo dentro de una transacción
         Reference savedReference = referenceService.save(newReference);
 
         return ResponseEntity
                 .created(URI.create("/api/references/" + savedReference.getId()))
                 .body(convertDTO.convertToReferenceDTO(savedReference));
     }
+
 
     @Transactional
     @PutMapping("/update/{referenceId}")
@@ -106,8 +133,24 @@ public class ReferenceController {
         existingReference.setDescription(updatedReferenceDTO.getDescription());
         existingReference.setImage(updatedReferenceDTO.getImage());
 
-        // Verificar y procesar los nuevos detalles del DTO
+        // Mapa para realizar un seguimiento de los detalles del JSON por producto
+        Map<String, ReferenceDetailDTO> productDetailsMap = new HashMap<>();
+
+        // Combinar cantidades de detalles con el mismo producto
         for (ReferenceDetailDTO detailDTO : updatedReferenceDTO.getDetails()) {
+            String productName = detailDTO.getProduct();
+            if (productDetailsMap.containsKey(productName)) {
+                // Si ya existe, sumar la cantidad al detalle existente
+                ReferenceDetailDTO existingDetailDTO = productDetailsMap.get(productName);
+                existingDetailDTO.setQuantity(existingDetailDTO.getQuantity() + detailDTO.getQuantity());
+            } else {
+                // Si no existe, agregar el detalle al mapa
+                productDetailsMap.put(productName, detailDTO);
+            }
+        }
+
+        // Verificar y procesar los detalles combinados del mapa
+        for (ReferenceDetailDTO detailDTO : productDetailsMap.values()) {
             Product product = productService.findByName(detailDTO.getProduct());
             if (product == null) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -116,20 +159,13 @@ public class ReferenceController {
 
             // Verificar si ya existe un detalle para el mismo producto
             String productName = detailDTO.getProduct();
-            ReferenceDetail existingDetail = null;
-
-            for (ReferenceDetail detail : existingReference.getDetail()) {
-                if (detail.getProduct().getName().equals(productName)) {
-                    existingDetail = detail;
-                    break;
-                }
-            }
+            ReferenceDetail existingDetail = getExistingDetail(existingReference, productName);
 
             if (existingDetail != null) {
-                // Actualizar la cantidad del detalle existente con la nueva cantidad
+                // Si existe, actualizar la cantidad del detalle existente con la nueva cantidad
                 existingDetail.setQuantity(detailDTO.getQuantity());
             } else {
-                // Crear un nuevo detalle y agregarlo a la lista de detalles
+                // Si no existe, crear un nuevo detalle y agregarlo a la lista
                 ReferenceDetail newDetail = convertDTO.convertToReferenceDetail(detailDTO, existingReference);
                 newDetail.setProduct(product);
                 existingReference.getDetail().add(newDetail);
@@ -138,35 +174,21 @@ public class ReferenceController {
 
         // Eliminar detalles que ya no están presentes
         existingReference.getDetail().removeIf(detail ->
-                updatedReferenceDTO.getDetails().stream()
+                productDetailsMap.values().stream()
                         .noneMatch(dto -> dto.getProduct().equals(detail.getProduct().getName())));
 
         // Guardar la referencia actualizada
         existingReference = referenceService.save(existingReference);
 
+
         return ResponseEntity.ok(convertDTO.convertToReferenceDTO(existingReference));
     }
 
-
-    private ResponseEntity<?> handleReferenceDetails(ReferenceDTO updatedReferenceDTO, Reference existingReference, Map<String, ReferenceDetail> productDetailsMap) {
-        for (ReferenceDetailDTO detailDTO : updatedReferenceDTO.getDetails()) {
-            Product product = productService.findByName(detailDTO.getProduct());
-            if (product == null) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body("El producto " + detailDTO.getProduct() + " en los detalles no existe");
-            }
-
-            // Verificar si ya existe un detalle para el mismo producto
-            String productName = detailDTO.getProduct();
-            ReferenceDetail existingDetail = productDetailsMap.get(productName);
-            if (existingDetail != null) {
-                // Sumar la cantidad al detalle existente
-                existingDetail.setQuantity(existingDetail.getQuantity() + detailDTO.getQuantity());
-            } else {
-                // Crear un nuevo detalle y agregarlo al mapa
-                ReferenceDetail newDetail = convertDTO.convertToReferenceDetail(detailDTO, existingReference);
-                newDetail.setProduct(product);
-                productDetailsMap.put(productName, newDetail);
+    // Método para obtener el detalle existente por nombre de producto
+    private ReferenceDetail getExistingDetail(Reference existingReference, String productName) {
+        for (ReferenceDetail detail : existingReference.getDetail()) {
+            if (detail.getProduct().getName().equals(productName)) {
+                return detail;
             }
         }
         return null;
--- src/main/java/com/chiton/api/dto/ReferenceDetailDTO.java
+++ src/main/java/com/chiton/api/dto/ReferenceDetailDTO.java
@@ -6,14 +6,11 @@ import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ReferenceDetailDTO implements Serializable {
 
-    @EqualsAndHashCode.Include
     final Long id;
 
-    @EqualsAndHashCode.Include
     final String product;
 
-    final Double quantity;
+    Double quantity;
 }
